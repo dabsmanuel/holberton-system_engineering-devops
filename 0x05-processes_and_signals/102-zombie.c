d687d83a0addd4ae2ad70233ccf18e81ad966655
mrdoomus <mrdoom.official@gmail.com>
2019-11-04 15:19:08 -0500

    Added advanced tasks

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - Loops infinitely with one second of step
+ * Return: An int
+**/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates 5 zombie processes
+ * Return: 1 if couldn't fork, 0 if eveyrthing is ok
+**/
+int main(void)
+{
+	int i;
+	pid_t zombie_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+		if (zombie_pid == -1)
+			return (1);
+		printf("Zombie process created, PID: %d\n", zombie_pid);
+	}
+
+	infinite_while();
+	return (0);
+}
