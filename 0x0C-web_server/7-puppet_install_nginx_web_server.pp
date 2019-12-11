32c29e890765f435e6fa0216ca2453f4d734bcb9
mrdoomus <mrdoom.official@gmail.com>
2019-12-11 17:58:42 -0500

    master: Fixed 7th script with missing semi colon

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,19 +2,16 @@
 package { 'nginx':
   ensure  => installed,
 }
-
 file { 'index.nginx-debian.html':
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Holberton School',
 }
-
 file_line { 'writes redirection site':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server',
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://findtheinvisiblecow.com permanent;',
 }
-
 service { 'nginx':
   ensure  => 'running',
   enable  => true,
