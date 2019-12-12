416bbf8dc5bf33f297f5619c4d1673a520585c28
mrdoomus <mrdoom.official@gmail.com>
2019-12-11 21:48:54 -0500

    master: Fixed order or instructions

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,16 +2,16 @@
 package { 'nginx':
   ensure  => installed,
 }
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School
+',
+}
 file_line { 'writes redirection site':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://findtheinvisiblecow.com permanent;',
-}
-file { 'index.nginx-debian.html':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Holberton School
-',
 }
 service { 'nginx':
   ensure  => 'running',
